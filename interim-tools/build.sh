8092d05066c599d843289b0c9849d160a0305e59
Gene Wood <gene_wood@cementhorizon.com>
2013-02-04 17:45:54 -0800

    initial commit of interim build and deploy tools

---
+++ interim-tools/build.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -x
+
+# This shabby script is supposed to hold us over for Q1 2013 while we spin up persona
+# in AWS. During that time this should enable jrgm to do build his own packages for staging deploys
+
+# This script assumes you're running it from the r6.build.mtv1.svc.mozilla.com build machine
+
+package=$1
+train=$2
+code_rev=$3
+locale_rev=$4
+
+if [ -z "$package" -o -z "$train" ]; then
+  echo "$0 PACKAGE TRAIN [CODE_REV] [LOCALE_REV]"
+  echo "$0 browserid train-2013.02.01 7a2f1e479a279246316a1ed5d23fe1c507c35b8e"
+  echo "$0 browserid-bigtent train-2013.02.01 7a2f1e479a279246316a1ed5d23fe1c507c35b8e"
+  exit 1
+fi
+
+if [ "$package" = "browserid" -o "$package" = "browserid_private" ]; then
+  rpmname="browserid-server"
+elif [ "$package" = "browserid-bigtent" ]; then
+  rpmname="browserid-bigtent"
+else
+  echo "Package \"$package\" not recognized. Aborting..."
+  exit 1
+fi
+
+mkdir -p ~/workspace/$package
+cd ~/workspace/$package
+git pull
+git checkout $train
+if [ "$locale_rev" ]; then
+  svn up -r $locale_rev locale
+else
+  svn up locale
+fi
+
+if [ -n "$code_rev" -a "`git rev-parse HEAD`" != "$code_rev" ]; then
+  echo "Required code_rev of $code_rev doesn't match current HEAD of `git rev-parse HEAD`. Aborting..."
+  exit 1
+fi
+
+if make rpm; then
+  echo "Success, next you'll need to distribute the RPM"
+else
+  echo "Build failed. Aborting..."
+  exit 1
+fi
+
+rm -rf /tmp/.npm
---
+++ interim-tools/distribute_and_install.sh
@@ -0,0 +1,158 @@
+#!/bin/bash
+
+# This shabby script is supposed to hold us over for Q1 2013 while we spin up persona
+# in AWS. During that time this should enable jrgm to do his own staging deploys
+
+# This script assumes you're running it from Mozilla's office network, either
+# VPNd into MPT or not
+
+package=$1
+train=$2
+
+if [ -z "$package" -o -z "$train" ]; then
+  echo "$0 PACKAGE TRAIN"
+  echo "$0 browserid train-2013.02.01"
+  echo "$0 browserid-bigtent train-2013.02.01"
+  exit 1
+fi
+
+if [ "$package" = "browserid" -o "$package" = "browserid_private" ]; then
+  rpmname="browserid-server"
+elif [ "$package" = "browserid-bigtent" ]; then
+  rpmname="browserid-bigtent"
+else
+  echo "Package $package not recognized. Aborting..."
+  exit 1
+fi
+
+if [ "$package" = "browserid-bigtent" ]; then
+  targets="bigtent-yahoo1.idweb bigtent-yahoo2.idweb"
+elif [ "$package" = "browserid" ]; then
+  SERVERS_web="web1.idweb web2.idweb web3.idweb"
+  SERVERS_sweb="sweb1.idsecweb sweb2.idsecweb sweb3.idsecweb"
+  SERVERS_sign="sign1.idkeysign sign2.idkeysign sign3.idkeysign"
+  SERVERS_all="$SERVERS_web $SERVERS_sweb $SERVERS_sign"
+  targets="$SERVERS_all"
+fi
+
+function fetch {
+  path="$1"
+  remotefile="$2"
+  ssh r6.build.mtv1.svc.mozilla.com "
+    cat \"$path/$remotefile\"
+  " > "$remotefile"
+  if [ ! -e "$remotefile" ]; then
+    exit 1
+  fi
+}
+
+function push {
+  adm=adm1.scl2.stage.svc.mozilla.com
+  jump=boris.mozilla.com
+  remotehost=$1
+  localfile=$2
+
+  if ping -c 1 -W 1 $remotehost >/dev/null; then
+    cat < $localfile | ssh $remotehost "
+      cd /home/`whoami` && cat > `basename $localfile`
+    "
+  elif ping -c 1 -W 1 $adm >/dev/null; then
+    cat < $localfile | ssh -A $adm "
+      ssh $remotehost \"
+        cd /home/`whoami` && cat > `basename $localfile`
+      \"
+    "
+  else
+    cat < $localfile | ssh -A $jump "
+      ssh -A $adm \"
+        ssh $remotehost \\\"
+          cd /home/`whoami` && cat > `basename $localfile`
+        \\\"
+      \"
+    "
+  fi
+}
+
+fullrpmname="`ssh r6.build.mtv1.svc.mozilla.com \"ls -t1 ~/workspace/$package/rpmbuild/RPMS/x86_64/$rpmname-*${train#train-}*.rpm | grep -v debug | head -1\"`"
+rpmfilename="`basename $fullrpmname`"
+echo "Fetching $rpmfilename"
+if ! fetch "`dirname $fullrpmname`" "$rpmfilename"; then
+  echo "Unable to fetch $rpmfilename. Aborting..."
+  exit 1
+fi
+
+echo "Preparing to distribute $rpmfilename"
+
+if [ "$package" = "browserid-bigtent" -o "$package" = "browserid" ]; then
+  echo "Distributing to adm1.scl2.stage.svc.mozilla.com"
+  push adm1.scl2.stage.svc.mozilla.com $rpmfilename
+fi
+
+if [ "$package" = "browserid" ]; then
+  echo "Distributing to adm1.scl2.svc.mozilla.com"
+  push adm1.scl2.svc.mozilla.com $rpmfilename
+
+  echo "Distributing to clientN QA loadtesting machines"
+  for i in `seq 4 9` `seq 11 30`; do
+    clientlist="$clientlist client${i}.scl2.svc.mozilla.com"
+  done
+
+  if ping -c 1 -W 1 client4.scl2.svc.mozilla.com >/dev/null; then
+    xapply -xP25 "scp $rpmfilename %1: 2>&1 | sed -e 's/^/%1: /'" $clientlist
+  else
+    ssh -A boris.mozilla.com "
+      ssh -A adm1.scl2.svc.mozilla.com \"
+        xapply -xP25 \\\"scp $rpmfilename %1: 2>&1 | sed -e 's/^/%1: /' \\\" $clientlist
+      \"
+    "
+  fi
+fi
+
+# This requires sudo on mrepo
+#echo "Distributing to mrepo1.dmz.scl3.mozilla.com"
+#push mrepo1.dmz.scl3.mozilla.com $rpmfilename
+#ssh -A boris.mozilla.com "ssh mrepo1.dmz.scl3.mozilla.com \"sudo install -m 0644 -o root -g root $rpmname /data/mrepo-src/6Server-x86_64/mozilla-services/ && sudo update-mrepo mozilla-services\""
+
+echo "Starting install to stage"
+# /usr/local/bin/install_browserid.sh
+# sysadmins r57717
+
+if ping -c 1 -W 1 adm1.scl2.stage.svc.mozilla.com >/dev/null; then
+  ssh -A adm1.scl2.stage.svc.mozilla.com "
+    xapply -P9 \"
+      scp $rpmfilename %1: && 
+      ssh %1 \\\"test -e $rpmfilename && 
+        sudo /usr/local/bin/install_browserid.sh $rpmfilename
+      \\\" 2>&1 | sed -e 's/^/%1: /'
+    \" $targets
+  "
+else
+  ssh -A boris.mozilla.com "
+    ssh -A adm1.scl2.stage.svc.mozilla.com \"
+      xapply -P9 \\\"
+        scp $rpmfilename %1: && 
+        ssh %1 \\\\\\\"test -e $rpmfilename && 
+          sudo /usr/local/bin/install_browserid.sh $rpmfilename
+        \\\\\\\" 2>&1 | sed -e 's/^/%1: /'
+      \\\" $targets
+    \"
+  "
+fi
+
+echo "Starting install to QA loadtesting clientN machines"
+if [ "$package" = "browserid" ]; then
+  if ping -c 1 -W 1 client4.scl2.svc.mozilla.com >/dev/null; then
+    xapply -P25 "ssh %1 \"test -e $rpmfilename && 
+      sudo /usr/local/bin/install_browserid.sh $rpmfilename\" 2>&1 | sed -e 's/^/%1: /'" $clientlist
+  else
+    ssh -A boris.mozilla.com "
+      ssh -A adm1.scl2.svc.mozilla.com \"
+        xapply -P25 \\\"
+          ssh %1 \\\\\\\"test -e $rpmfilename && 
+            sudo /usr/local/bin/install_browserid.sh $rpmfilename
+          \\\\\\\" 2>&1 | sed -e 's/^/%1: /'
+        \\\" $clientlist
+      \"
+    "
+  fi
+fi
