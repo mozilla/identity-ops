6e343585cefce7b41807e56bc9e16b08a56a9769
Gene Wood <gene_wood@cementhorizon.com>
2013-03-19 09:59:22 -0700

    adding access cookbook to grant qa access https://bugzilla.mozilla.org/show_bug.cgi?id=852373

---
+++ chef/cookbooks/access/recipes/default.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: access
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+directory "/root/.ssh" do
+  owner "root"
+  group "root"
+  mode 0700
+end
+
+file "/root/.ssh/authorized_keys" do
+  source "root/.ssh/authorized_keys"
+  owner "root"
+  group "root"
+  mode 0600
+end
