fbda278745d34051dc199b02d25ec1b6cd1b32ff
Gene Wood <gene_wood@cementhorizon.com>
2013-04-03 13:05:24 -0700

    initial commit of keysign cookbook

---
+++ chef/cookbooks/persona-keysign/CHANGELOG.md
@@ -0,0 +1,12 @@
+# CHANGELOG for persona-keysign
+
+This file is used to list changes made in each version of persona-keysign.
+
+## 0.1.0:
+
+* Initial release of persona-keysign
+
+- - -
+Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
+
+The [Github Flavored Markdown page](http://github.github.com/github-flavored-markdown/) describes the differences between markdown on github and standard markdown.
---
+++ chef/cookbooks/persona-keysign/recipes/default.rb
@@ -0,0 +1,78 @@
+#
+# Cookbook Name:: persona-keysign
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "persona-common::default"
+include_recipe "persona-common::daemontools"
+
+rpms = [node[:persona][:keysign][:rpms]["browserid-server"],
+        node[:persona][:keysign][:rpms][:nodejs]]
+
+for rpm in rpms do
+  remote_file "#{Chef::Config[:file_cache_path]}/#{rpm}" do
+     source "https://s3.amazonaws.com/mozilla-identity-us-standard/rpms/#{rpm}"
+ end
+end
+
+package "nodejs" do
+  source "#{Chef::Config[:file_cache_path]}/#{node[:persona][:keysign][:rpms][:nodejs]}"
+end
+
+package "browserid-server" do
+  source "#{Chef::Config[:file_cache_path]}/#{node[:persona][:keysign][:rpms]["browserid-server"]}"
+end
+
+template "/opt/browserid/config/production.json" do
+  source "opt/browserid/config/production.json.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  notifies :restart, "daemontools_service[browserid-keysigner]", :delayed
+end
+
+file "/var/browserid/browserid_cookie.sekret" do
+  content node[:persona][:cookie_sekret]
+  mode 0640
+  user "root"
+  group "browserid"
+  notifies :restart, "daemontools_service[browserid-keysigner]", :delayed
+end
+
+file "/var/browserid/root.cert" do
+  content node[:persona][:root_cert]
+  mode 0644
+  user "root"
+  group "browserid"
+  notifies :restart, "daemontools_service[browserid-keysigner]", :delayed
+end
+
+file "/var/browserid/root.secretkey" do
+  content node[:persona][:root_secretkey]
+  mode 0640
+  user "root"
+  group "browserid"
+  notifies :restart, "daemontools_service[browserid-keysigner]", :delayed
+end
+
+
+daemontools_service "browserid-keysigner" do
+  directory "/var/services/browserid-keysigner"
+  template "browserid-keysigner"
+  action [:enable, :start]
+  log true
+end
+
+include_recipe "persona-common::nginx"
+
+cookbook_file "/etc/nginx/conf.d/idkeysign.conf" do
+  source "etc/nginx/conf.d/idkeysign.conf"
+  owner "root"
+  group "root"
+  mode 0644
+  notifies :restart, "daemontools_service[nginx]", :delayed
+end
