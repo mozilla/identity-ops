4419593199b2d6d317b40e6d22befe9292667798
Gene Wood <gene_wood@cementhorizon.com>
2013-04-05 08:24:05 -0700

    missed adding the default attributes

---
+++ chef/cookbooks/persona-dbwrite/attributes/default.rb
@@ -0,0 +1,6 @@
+default["persona"]["dbwrite"]["rpms"]["browserid-server"] = "browserid-server-0.2013.03.01-2.el6_113972.x86_64.rpm"
+default["persona"]["dbwrite"]["rpms"]["nodejs"] = "nodejs-0.8.17-1.el6.x86_64.rpm"
+
+default["persona"]["dbwrite"]["database"]["host"] = "dbmaster-example-123456.us-west-2.elb.amazonaws.com"
+default["persona"]["dbwrite"]["database"]["user"] = "browseridrw"
+default["persona"]["dbwrite"]["database"]["password"] = "password-goes-here"
