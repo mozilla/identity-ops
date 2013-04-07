ba638c5495c1492070f9bdb0c997481df3dce1e9
Gene Wood <gene_wood@cementhorizon.com>
2013-04-07 11:09:15 -0700

    initial dbread cookbook commit

---
+++ chef/cookbooks/persona-dbread/CHANGELOG.md
@@ -0,0 +1,12 @@
+# CHANGELOG for persona-dbread
+
+This file is used to list changes made in each version of persona-dbread.
+
+## 0.1.0:
+
+* Initial release of persona-dbread
+
+- - -
+Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
+
+The [Github Flavored Markdown page](http://github.github.com/github-flavored-markdown/) describes the differences between markdown on github and standard markdown.
---
+++ chef/cookbooks/persona-dbread/recipes/default.rb
@@ -0,0 +1,108 @@
+#
+# Cookbook Name:: persona-dbread
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+group "mysql" do
+  gid node[:persona][:mysql_uid]
+end
+
+user "mysql" do
+  comment "MySQL User"
+  uid node[:persona][:mysql_uid]
+  gid node[:persona][:mysql_uid]
+  home "/data/mysql"
+  supports :manage_home => true
+end
+
+directory "/data" do
+  owner "root"
+  group "root"
+  mode 0755
+end
+
+directory "/data/mysql" do
+  owner "mysql"
+  group "mysql"
+  mode 0750
+end
+
+# Note that installing Percona-Server-shared-51 first
+# is required as it's a dependency for the other
+# Percona packages
+# Note that Percona-Server-shared-51 will remove
+# the mysql-libs package
+# Note I'm not adding in Percona-Server-shared-compat
+# as it conflicts with Percona-Server-shared-51 and
+# to include it will require breaking the yum depenency
+# model on the system
+for pkg in ["mha4mysql-node",
+            "percona-toolkit",
+            "Percona-Server-shared-51",
+            "Percona-Server-client-51",
+            "Percona-Server-server-51",
+            "Percona-Server-devel-51"] do
+  remote_file "#{Chef::Config[:file_cache_path]}/#{node[:persona][:dbread][:rpms][pkg]}" do
+    source "https://s3.amazonaws.com/mozilla-identity-us-standard/rpms/#{node[:persona][:dbread][:rpms][pkg]}"
+  end
+  package pkg do
+    source "#{Chef::Config[:file_cache_path]}/#{node[:persona][:dbread][:rpms][pkg]}"
+  end
+end
+
+service "mysql" do
+  action :enable
+end
+
+cookbook_file "/etc/init.d/mysql" do
+  # Original Percona init script is at
+  # files/default/etc/init.d/mysql.orig
+  source "etc/init.d/mysql"
+  owner "root"
+  group "root"
+  mode 0755
+  notifies :restart, "service[mysql]", :delayed
+end
+
+template "/etc/my.cnf" do
+  source "etc/my.cnf.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  # :innodb_buffer_pool_size : 75% of total memory
+  # :server_id : ip address in integer form ( http://dev.mysql.com/doc/refman/5.1/en/replication-options.html#option_mysqld_server-id )
+  variables(:innodb_buffer_pool_size => ((node[:memory][:total][0..-3].
+              to_i) * 0.75).to_i.to_s + node[:memory][:total][-2..-1],
+            :server_id => node[:ipaddress].split('.').collect(&:to_i).
+              pack('C*').unpack('N').first)
+  notifies :restart, "service[mysql]", :delayed
+end
+
+file "/var/log/mysql-slow.log" do
+  owner "mysql"
+  group "mysql"
+  mode 0644
+end
+
+# I'm unsure what this is used for and if we need it
+# I assume it relates to mha4mysql-node
+directory "/var/masterha" do
+  owner "root"
+  group "root"
+  mode 0750
+end
+
+daemontools_service "heartbeat-browserid" do
+  directory "/var/services/heartbeat-browserid"
+  template "heartbeat-browserid"
+  action [:enable, :start]
+  log true
+end
+
+service "mysql" do
+  action :start
+end
