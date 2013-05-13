97b7c5fa295e8c8662262ab6baa37df2115c785a
Gene Wood <gene_wood@cementhorizon.com>
2013-05-12 21:36:32 -0700

    Adding hostname setting recipe

--- chef/cookbooks/persona-common/recipes/default.rb
+++ chef/cookbooks/persona-common/recipes/default.rb
@@ -7,6 +7,14 @@
 # All rights reserved - Do Not Redistribute
 #
 
+# Because "chef_gem" occurs during the compile phase (not the convergence phase)
+# so that the gem can be used inside the current chef run, and because the "aws-sdk"
+# gem depends on libxml2 and libxslt, I need to install those two packages during
+# the compile phase, instead of the convergence phase. This is done by creating the
+# resource and then calling run_action on it explicitly instead of waiting for convergence
+[package("libxml2-devel"), package("libxslt-devel")].each { |pkg| pkg.run_action(:install) }
+
+include_recipe "persona-common::hostname"
 include_recipe "persona-common::daemontools"
 include_recipe "persona-common::iptables"
 
@@ -25,10 +33,4 @@ cookbook_file "/etc/sysconfig/yum-autoupdate" do
   mode 0644
 end
 
-# Because "chef_gem" occurs during the compile phase (not the convergence phase)
-# so that the gem can be used inside the current chef run, and because the "aws-sdk"
-# gem depends on libxml2 and libxslt, I need to install those two packages during
-# the compile phase, instead of the convergence phase. This is done by creating the
-# resource and then calling run_action on it explicitly instead of waiting for convergence
-[package("libxml2-devel"), package("libxslt-devel")].each { |pkg| pkg.run_action(:install) }
 chef_gem "aws-sdk"
---
+++ chef/cookbooks/persona-common/recipes/hostname.rb
@@ -0,0 +1,69 @@
+#
+# Cookbook Name:: persona-common
+# Recipe:: hostname
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+# This is all done at compile time and not during convergence so that the 
+# new node[:fqdn] is available on the first run for use. To accomplish this
+# we must 1) add the fqdn to /etc/hosts 2) update the hostname with the
+# hosname command 3) reload ohai
+# Ohai determines the node[:fqdn] by running "hostname --fqdn" which depends
+# on 1) and 2)
+#
+# Inspiration from
+# https://github.com/opscode-cookbooks/ohai/blob/master/recipes/default.rb
+
+new_hostname="ip-#{node[:ipaddress].tr('.','-')}" + 
+             (node[:tier] != nil ? ".#{node[:tier]}" : "") + 
+             (node[:stack][:name] != nil ? ".#{node[:stack][:name]}" : "") +  
+             (node[:stack][:type] != nil ? ".#{node[:stack][:type]}" : "") +
+             (node[:aws_region] != nil ? ".#{node[:aws_region]}" : "") +
+             ".allizomaws.com"
+reload_ohai=false
+
+res = template "/etc/hosts" do
+        source "etc/hosts.erb"
+        variables(
+          :hostname => new_hostname
+        )
+        user "root"
+        group "root"
+        mode 0644
+      end
+res.run_action(:create)
+reload_ohai ||= res.updated?
+
+res = execute "hostname #{new_hostname}" do
+        not_if {node[:fqdn] == new_hostname}
+      end
+res.run_action(:run)
+reload_ohai ||= res.updated?
+
+if reload_ohai then
+  res = ohai "reload"
+  res.run_action(:reload)
+end
+
+ruby_block "inject_hostname" do
+  block do
+    f = Chef::Util::FileEdit.new("/etc/sysconfig/network")
+    f.search_file_replace_line(/^HOSTNAME=/, "HOSTNAME=#{new_hostname}")
+    f.write_file
+  end
+  not_if do
+    open('/etc/sysconfig/network') { |f| f.lines.find { |line| line.include?("HOSTNAME=#{new_hostname}") } }
+  end
+end
+
+# template "/etc/sysconfig/network" do
+  # source "etc/sysconfig/network.erb"
+  # variables(
+    # :hostname => new_hostname
+  # )
+  # user "root"
+  # group "root"
+  # mode 0644
+# end
