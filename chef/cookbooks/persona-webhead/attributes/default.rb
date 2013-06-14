5b629b486ebdf3c30dbfe0ea9a97b3ea417a7c68
Gene Wood <gene_wood@cementhorizon.com>
2013-06-14 15:34:03 -0700

    Updating default rpm for train-2013.06.05-2 https://bugzilla.mozilla.org/show_bug.cgi?id=883391

--- chef/cookbooks/persona-webhead/attributes/default.rb
+++ chef/cookbooks/persona-webhead/attributes/default.rb
@@ -1,4 +1,4 @@
-default["persona"]["webhead"]["rpms"]["browserid-server"] = "browserid-server-0.2013.06.05-1.el6_116771.x86_64.rpm"
+default["persona"]["webhead"]["rpms"]["browserid-server"] = "browserid-server-0.2013.06.05-2.el6_116934.x86_64.rpm"
 default["persona"]["webhead"]["rpms"]["nodejs"] = "nodejs-0.8.24-1.el6.x86_64.rpm"
 default["persona"]["loadtest"] = false
 
