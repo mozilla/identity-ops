70bac81c4c290b17b21e190d3602e0a36a161270
Gene Wood <gene_wood@cementhorizon.com>
2013-06-28 12:48:11 -0700

    initial commit of identity-gateway

---
+++ chef/cookbooks/identity-gateway/CHANGELOG.md
@@ -0,0 +1,12 @@
+# CHANGELOG for identity-gateway
+
+This file is used to list changes made in each version of identity-gateway.
+
+## 0.1.0:
+
+* Initial release of identity-gateway
+
+- - -
+Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
+
+The [Github Flavored Markdown page](http://github.github.com/github-flavored-markdown/) describes the differences between markdown on github and standard markdown.
---
+++ chef/cookbooks/identity-gateway/recipes/default.rb
@@ -0,0 +1,50 @@
+#
+# Cookbook Name:: identity-gateway
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "httpd"
+package "php"
+
+service "httpd" do
+  action [:start, :enable]
+end
+
+for rpm in ["yajl-2.0.4-2.el6.x86_64.rpm", "mod_auth_browserid-0.1-1.el6.x86_64.rpm"] do
+  remote_file "#{Chef::Config[:file_cache_path]}/#{rpm}" do
+    source "https://s3.amazonaws.com/mozilla-identity-us-standard/rpms/#{rpm}"
+  end
+end
+
+package "yajl" do
+  source "#{Chef::Config[:file_cache_path]}/yajl-2.0.4-2.el6.x86_64.rpm"
+end
+
+package "mod_auth_browserid" do
+  source "#{Chef::Config[:file_cache_path]}/mod_auth_browserid-0.1-1.el6.x86_64.rpm"
+  notifies :restart, "service[httpd]", :delayed
+end
+
+for file in ["var/www/html/browserid_login.php",
+             "var/www/html/persona_sign_in_blue.png",
+             "var/www/mod_browserid_users",
+             "etc/httpd/conf.d/graphite-web.conf"] do
+  cookbook_file "/#{file}" do
+    source file
+    owner "root"
+    group "root"
+    mode 0644
+  end
+end
+
+template "/etc/httpd/conf.d/identity-gateway.conf" do
+  source "etc/httpd/conf.d/identity-gateway.conf.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  notifies :restart, "service[httpd]", :delayed
+end
