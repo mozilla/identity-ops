50607dfda014c94729231226a9969b80a4653664
Gene Wood <gene_wood@cementhorizon.com>
2013-07-04 08:51:40 -0700

    Updating get_hosts with more functionality

--- chef/cookbooks/persona-admin/recipes/default.rb
+++ chef/cookbooks/persona-admin/recipes/default.rb
@@ -10,6 +10,23 @@
 include_recipe "persona-common::default"
 #include_recipe "persona-db::monitor"
 
+# Install the Rackspace IUS repo to get python 2.7
+remote_file "#{Chef::Config[:file_cache_path]}/ius-release-1.0-11.ius.centos6.noarch.rpm" do
+  source "http://dl.iuscommunity.org/pub/ius/stable/CentOS/6/x86_64/ius-release-1.0-11.ius.centos6.noarch.rpm"
+end
+package pkg do
+  source "#{Chef::Config[:file_cache_path]}/ius-release-1.0-11.ius.centos6.noarch.rpm"
+end
+
+package "pssh"
+
+package "python27"
+package "python27-distribute"
+
+easy_install_package "boto" do
+  easy_install_binary "/usr/bin/easy_install-2.7"
+end
+
 cookbook_file "/usr/local/bin/get_hosts" do
   source "usr/local/bin/get_hosts"
   owner "root"
