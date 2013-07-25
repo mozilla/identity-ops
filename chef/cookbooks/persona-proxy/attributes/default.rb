45eb98a5459b3744a91d7d2a5e9806b57eaa7134
Gene Wood <gene_wood@cementhorizon.com>
2013-07-25 13:00:28 -0700

    Upgrading squid to fix init script bug

--- chef/cookbooks/persona-proxy/attributes/default.rb
+++ chef/cookbooks/persona-proxy/attributes/default.rb
@@ -1,5 +1,3 @@
 default["persona"]["proxy"]["kpi_target_host"] = "kpiggybank.example.com"
 default["persona"]["proxy"]["kpi_target_cidr"] = "1.2.3.4/32"
-default["persona"]["proxy"]["rpms"]["squid"] = "squid-3.3.5-1.el6.x86_64.rpm"
-
-
+default["persona"]["proxy"]["rpms"]["squid"] = "squid-3.3.8-1.el6.x86_64.rpm"
