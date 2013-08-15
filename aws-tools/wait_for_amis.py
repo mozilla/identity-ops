f52ecd4b681e0b122ac340ab0d35c7c08ee67967
Gene Wood <gene_wood@cementhorizon.com>
2013-08-15 12:14:53 -0700

    Enable passing in a git hash instead of a list of AMIs

--- aws-tools/wait_for_amis.py
+++ aws-tools/wait_for_amis.py
@@ -4,7 +4,17 @@ import sys
 import time
 region='us-west-2'
 conn_ec2 = boto.ec2.connect_to_region(region)
-for id in sys.argv[1:]:
+images = conn_ec2.get_all_images()
+if sys.argv[1][0:3] != 'ami':
+  print "searching for hash %s" % sys.argv[1]
+  ids = [x.id for x in images if sys.argv[1] in str(x.name)]
+else:
+  ids = sys.argv[1:]
+
+print "ids are '%s'" % ids
+
+for id in ids:
   while conn_ec2.get_image(id).state != 'available':
+    print "waiting on %s" % id
     time.sleep(10)
   print "%s is available" % id
