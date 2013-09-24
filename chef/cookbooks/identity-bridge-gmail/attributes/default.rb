12d2a674fd847b978099016530fa7157f9c1166c
Gene Wood <gene_wood@cementhorizon.com>
2013-09-24 15:55:49 -0700

    New bridge-gmail build https://bugzilla.mozilla.org/show_bug.cgi?id=920260

--- chef/cookbooks/identity-bridge-gmail/attributes/default.rb
+++ chef/cookbooks/identity-bridge-gmail/attributes/default.rb
@@ -11,5 +11,5 @@ default["browserid-bridge-gmail"]["publickey"] = "{\"algorithm\":\"RS\",\"n\":\"
 default["proxy"]["host"] = false
 default["proxy"]["port"] = false
 
-default["browserid-bridge-gmail"]["rpms"]["browserid-bridge-gmail"] = 'browserid-bridge-gmail-0.9.2-0.1.20130718SHAb18e22eR118029.el6.x86_64.rpm'
+default["browserid-bridge-gmail"]["rpms"]["browserid-bridge-gmail"] = 'browserid-bridge-gmail-0.9.3-0.1.20130924SHA183f3a9R118029.el6.x86_64.rpm'
 default["browserid-bridge-gmail"]["rpms"]["nodejs"] = 'nodejs-0.8.24-1.el6.x86_64.rpm'
