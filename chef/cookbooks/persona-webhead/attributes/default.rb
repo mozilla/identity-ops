55e483a64ba107ab1617f98a416cd20f150769a7
Gene Wood <gene_wood@cementhorizon.com>
2013-09-30 13:00:37 -0400

    new bigtent and persona https://bugzilla.mozilla.org/show_bug.cgi?id=921897 https://bugzilla.mozilla.org/show_bug.cgi?id=921956

--- chef/cookbooks/persona-webhead/attributes/default.rb
+++ chef/cookbooks/persona-webhead/attributes/default.rb
@@ -1,4 +1,4 @@
-default["persona"]["webhead"]["rpms"]["browserid-server"] = "browserid-server-0.2013.08.28-2.el6_119789.x86_64.rpm"
+default["persona"]["webhead"]["rpms"]["browserid-server"] = "browserid-server-0.2013.08.28-3.el6_119789.x86_64.rpm"
 default["persona"]["webhead"]["rpms"]["nodejs"] = "nodejs-0.8.24-1.el6.x86_64.rpm"
 default["persona"]["loadtest"] = false
 
