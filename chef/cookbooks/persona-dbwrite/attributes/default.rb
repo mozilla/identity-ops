6622e529688246d702e39d5849adf003c4711163
Gene Wood <gene_wood@cementhorizon.com>
2013-11-07 13:31:06 -0800

    train 2013.10.23 https://bugzilla.mozilla.org/show_bug.cgi?id=930783

--- chef/cookbooks/persona-dbwrite/attributes/default.rb
+++ chef/cookbooks/persona-dbwrite/attributes/default.rb
@@ -1,4 +1,4 @@
-default["persona"]["dbwrite"]["rpms"]["browserid-server"] = "browserid-server-0.2013.10.09-1.el6_121508.x86_64.rpm"
+default["persona"]["dbwrite"]["rpms"]["browserid-server"] = "browserid-server-0.2013.10.23-1.el6_122101.x86_64.rpm"
 default["persona"]["dbwrite"]["rpms"]["nodejs"] = "nodejs-0.8.26-1.el6.x86_64.rpm"
 
 default["persona"]["dbwrite"]["database"]["host"] = "dbmaster-example-123456.us-west-2.elb.amazonaws.com"
