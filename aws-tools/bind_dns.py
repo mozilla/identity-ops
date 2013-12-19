f83e677ee05e629736fa1fdf5c19622cd48b0222
Gene Wood <gene_wood@cementhorizon.com>
2013-12-19 15:12:19 -0800

    Redesign bind_dns to instead of binding a CloudFormation output variable to a DNS name in Route53 as a CNAME, it now binds a CloudFormation defined ELB to a DNS name in Route 53 as an alias.

--- aws-tools/bind_dns.py
+++ aws-tools/bind_dns.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
-"""Bind a DNS name to a CloudFormation stack output variable
+"""Bind a DNS name to a CloudFormation generated ELB
 
-usage: bind_dns.py [-h] [-d] FQDN REGION [STACKNAME] [VARIABLE]
+usage: bind_dns.py [-h] [-d] FQDN REGION [STACKNAME] [ELBNAME]
 
-Bind a DNS name to a CloudFormation stack output variable
+Bind a DNS name to a CloudFormation generated ELB
 
 positional arguments:
-  FQDN         Fully qualified DNS Name to CNAME to the stack output variable
+  FQDN         Fully qualified DNS Name to alias to the ELB
   REGION       AWS region containing CloudFormation stack
   STACKNAME    Name of the CloudFormation stack
-  VARIABLE     The CloudFormation stack output variable name
+  ELBNAME      The CloudFormation resource logical ID for the ELB as defined
+               in the CloudFormation template
 
 optional arguments:
   -h, --help   show this help message and exit
@@ -20,19 +21,22 @@ Examples :
 Show all CloudFormation stacks in us-east-1 :
   ./bind_dns.py foo.example.com us-east-1
 
-Show all CloudFormation output variables in stack foostack :
+Show all CloudFormation ELBs in stack foostack :
   ./bind_dns.py foo.example.com us-east-1 foostack
 
-Bind the DNS name foo.example.com to the CloudFormation output variable 
-called ELBName for the stack foostack :
-  ./bind_dns.py foo.example.com us-east-1 foostack ELBName
+Bind the DNS name foo.example.com to the CloudFormation ELB 
+called ExampleELB for the stack foostack :
+  ./bind_dns.py foo.example.com us-east-1 foostack ExampleELB
 
+Note : The ELBNAME value is the CloudFormation resource Logical ID
+(e.g. ExampleELB) not the Physical ID (e.g. mystack-ExampleE-PACIV6OTHJA1)
 
 """
 
 import argparse
 import boto
 import boto.ec2
+import boto.ec2.elb
 import boto.cloudformation
 import boto.route53.record
 import logging
@@ -50,30 +54,32 @@ epilog="""Examples :
 Show all CloudFormation stacks in us-east-1 :
   %(name)s foo.example.com us-east-1
 
-Show all CloudFormation output variables in stack foostack :
+Show all CloudFormation ELBs in stack foostack :
   %(name)s foo.example.com us-east-1 foostack
 
-Bind the DNS name foo.example.com to the CloudFormation output variable 
-called ELBName for the stack foostack :
-  %(name)s foo.example.com us-east-1 foostack ELBName
+Bind the DNS name foo.example.com to the CloudFormation ELB 
+called ExampleELB for the stack foostack :
+  %(name)s foo.example.com us-east-1 foostack ExampleELB
+
+Note : The ELBNAME value is the CloudFormation resource Logical ID
+(e.g. ExampleELB) not the Physical ID (e.g. mystack-ExampleE-PACIV6OTHJA1)
 
 """ % {'name': sys.argv[0]}
 
 parser = argparse.ArgumentParser(
                  description=('Bind a DNS name to a '
-                              'CloudFormation stack output '
-                              'variable'),
+                              'CloudFormation generated ELB'),
                  formatter_class=argparse.RawDescriptionHelpFormatter,
                  epilog=epilog)
 parser.add_argument('fqdn', metavar="FQDN",
-                    help='Fully qualified DNS Name to CNAME to the stack '
-                    'output variable')
+                    help='Fully qualified DNS Name to alias to the ELB')
 parser.add_argument('region', metavar="REGION", choices=all_regions, 
                     help='AWS region containing CloudFormation stack')
 parser.add_argument('stackname', metavar='STACKNAME', nargs="?", default=None,
                     help='Name of the CloudFormation stack')
-parser.add_argument('variable', metavar='VARIABLE', nargs="?", default=None,
-                    help='The CloudFormation stack output variable name')
+parser.add_argument('elbname', metavar='ELBNAME', nargs="?", default=None,
+                    help='The CloudFormation resource logical ID for the ELB '
+                    'as defined in the CloudFormation template')
 parser.add_argument('-d', '--debug', action="store_true",
                     help="Output debug information")
 args = parser.parse_args()
@@ -96,11 +102,15 @@ if len(stacks) == 0:
            ', '.join([x.stack_name for x in conn_cfn.describe_stacks()])))
 
 # This assumes we don't get multiple stack results from our search
-output = [x for x in stacks[0].outputs if x.key == args.variable]
-if len(output) == 0:
-    parser.error("argument VARIABLE: invalid choice: %s (choose from %s)" 
-        % (args.variable, 
-           ', '.join([x.key for x in stacks[0].outputs])))
+stack_resources = conn_cfn.describe_stack_resources(stacks[0].stack_name)
+stack_elbs = [x for x in stack_resources 
+        if x.resource_type == 'AWS::ElasticLoadBalancing::LoadBalancer' 
+        and x.logical_resource_id == args.elbname]
+if len(stack_elbs) == 0:
+    parser.error("argument ELBNAME: invalid choice: %s (choose from %s)" 
+        % (args.elbname, 
+           ', '.join([x.logical_resource_id for x in stack_resources
+                      if x.resource_type == 'AWS::ElasticLoadBalancing::LoadBalancer'])))
 
 # This assumes there aren't multiple outputs with the same name
 
@@ -119,29 +129,41 @@ if not zone_id:
                  "these zones : %s)" 
                  % (args.fqdn, [x['Name'] for x in all_zones]))
 
+conn_elb = boto.ec2.elb.connect_to_region(args.region)
+elb = conn_elb.get_all_load_balancers([stack_elbs[0].physical_resource_id])
+canonical_hosted_zone_elb_id = elb[0].canonical_hosted_zone_name_id
+canonical_hosted_zone_name = elb[0].canonical_hosted_zone_name
+
 # Adding/Updating record
 changes = boto.route53.record.ResourceRecordSets(conn_route53, 
                  zone_id, 
                  "Adding/Updating %s to %s in zone %s for stack %s "
-                 % (args.fqdn, output[0].value, zone_name, args.stackname))
+                 % (args.fqdn, canonical_hosted_zone_name, zone_name, args.stackname))
 matching_rrsets = [x for x in conn_route53.get_all_rrsets(zone_id) 
-                   if x.type == 'CNAME' and x.name == args.fqdn]
+                   if x.type == 'A' and x.name == args.fqdn]
+
 
 ttl = 30
 if len(matching_rrsets) == 1:
     logging.debug("Target DNS name %s exists already, deleting" 
                   % matching_rrsets[0].name)
     ttl = matching_rrsets[0].ttl
-    record = changes.add_change("DELETE", 
-                                matching_rrsets[0].name, 
-                                matching_rrsets[0].type,
-                                ttl=matching_rrsets[0].ttl)
-    record.add_value(matching_rrsets[0].resource_records[0])
-record = changes.add_change("CREATE", args.fqdn, 'CNAME', ttl)
-record.add_value(output[0].value)
+    record = changes.add_change(action="DELETE", 
+                                name=matching_rrsets[0].name, 
+                                type=matching_rrsets[0].type,
+                                ttl=matching_rrsets[0].ttl,
+                                alias_dns_name=matching_rrsets[0].alias_dns_name,
+                                alias_hosted_zone_id=matching_rrsets[0].alias_hosted_zone_id)
+
+record = changes.add_change(action="CREATE", 
+                            name=args.fqdn, 
+                            type='A', 
+                            ttl=ttl,
+                            alias_dns_name=canonical_hosted_zone_name,
+                            alias_hosted_zone_id=canonical_hosted_zone_elb_id)
 
 commit = changes.commit()
-logging.debug('Commiting DNS change %s' % commit)
+logging.debug('Committing DNS change %s' % commit)
 
 change = conn_route53.get_change(get_change_id(commit
                                    ['ChangeResourceRecordSetsResponse']))
