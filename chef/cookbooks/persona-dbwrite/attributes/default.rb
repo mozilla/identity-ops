18e111e7856ad38335af6615bf55e5bc501f07ea
Gene Wood <gene_wood@cementhorizon.com>
2014-04-22 15:21:56 -0700

    New app version from https://bugzilla.mozilla.org/show_bug.cgi?id=998743

--- chef/cookbooks/persona-dbwrite/attributes/default.rb
+++ chef/cookbooks/persona-dbwrite/attributes/default.rb
@@ -1,4 +1,4 @@
-default["persona"]["dbwrite"]["rpms"]["browserid-server"] = "browserid-server-0.2013.10.09-3.el6_121508.x86_64.rpm"
+default["persona"]["dbwrite"]["rpms"]["browserid-server"] = "browserid-server-0.2014.04.16-1.el6_126942.x86_64.rpm"
 default["persona"]["dbwrite"]["rpms"]["nodejs"] = "nodejs-0.8.26-1.el6.x86_64.rpm"
 
 default["persona"]["dbwrite"]["database"]["host"] = "dbmaster-example-123456.us-west-2.elb.amazonaws.com"
