c11407882dffafaf4c3011225ae3498dac692650
Gene Wood <gene_wood@cementhorizon.com>
2014-05-27 13:22:30 -0700

    Reverting security policy to v1.2 to workaround AWS bug ( https://bugzilla.mozilla.org/show_bug.cgi?id=958719#c2 )

--- aws-tools/stack_control.py
+++ aws-tools/stack_control.py
@@ -159,9 +159,71 @@ def create_stack(region,
                         "RC4-MD5": False,
                         "RC4-SHA": True,
                         "SEED-SHA": False}
-
-
     policy_name = 'Mozilla-Security-Assurance-Ciphersuite-Policy-v-2-0'
+
+    policy_attributes = {"ADH-AES128-SHA": False,
+                        "ADH-AES256-SHA": False,
+                        "ADH-CAMELLIA128-SHA": False,
+                        "ADH-CAMELLIA256-SHA": False,
+                        "ADH-DES-CBC3-SHA": False,
+                        "ADH-DES-CBC-SHA": False,
+                        "ADH-RC4-MD5": False,
+                        "ADH-SEED-SHA": False,
+                        "AES128-SHA": True,
+                        "AES256-SHA": True,
+                        "CAMELLIA128-SHA": True,
+                        "CAMELLIA256-SHA": True,
+                        "DES-CBC3-MD5": False,
+                        "DES-CBC3-SHA": False,
+                        "DES-CBC-MD5": False,
+                        "DES-CBC-SHA": False,
+                        "DHE-DSS-AES128-SHA": True,
+                        "DHE-DSS-AES256-SHA": True,
+                        "DHE-DSS-CAMELLIA128-SHA": False,
+                        "DHE-DSS-CAMELLIA256-SHA": False,
+                        "DHE-DSS-SEED-SHA": False,
+                        "DHE-RSA-AES128-SHA": False,
+                        "DHE-RSA-AES256-SHA": False,
+                        "DHE-RSA-CAMELLIA128-SHA": False,
+                        "DHE-RSA-CAMELLIA256-SHA": False,
+                        "DHE-RSA-SEED-SHA": False,
+                        "EDH-DSS-DES-CBC3-SHA": False,
+                        "EDH-DSS-DES-CBC-SHA": False,
+                        "EDH-RSA-DES-CBC3-SHA": False,
+                        "EDH-RSA-DES-CBC-SHA": False,
+                        "EXP-ADH-DES-CBC-SHA": False,
+                        "EXP-ADH-RC4-MD5": False,
+                        "EXP-DES-CBC-SHA": False,
+                        "EXP-EDH-DSS-DES-CBC-SHA": False,
+                        "EXP-EDH-RSA-DES-CBC-SHA": False,
+                        "EXP-KRB5-DES-CBC-MD5": False,
+                        "EXP-KRB5-DES-CBC-SHA": False,
+                        "EXP-KRB5-RC2-CBC-MD5": False,
+                        "EXP-KRB5-RC2-CBC-SHA": False,
+                        "EXP-KRB5-RC4-MD5": False,
+                        "EXP-KRB5-RC4-SHA": False,
+                        "EXP-RC2-CBC-MD5": False,
+                        "EXP-RC4-MD5": False,
+                        "IDEA-CBC-SHA": False,
+                        "KRB5-DES-CBC3-MD5": False,
+                        "KRB5-DES-CBC3-SHA": False,
+                        "KRB5-DES-CBC-MD5": False,
+                        "KRB5-DES-CBC-SHA": False,
+                        "KRB5-RC4-MD5": False,
+                        "KRB5-RC4-SHA": False,
+                        "Protocol-SSLv2": False,
+                        "Protocol-SSLv3": True,
+                        "Protocol-TLSv1": True,
+                        "PSK-3DES-EDE-CBC-SHA": False,
+                        "PSK-AES128-CBC-SHA": False,
+                        "PSK-AES256-CBC-SHA": False,
+                        "PSK-RC4-SHA": False,
+                        "RC2-CBC-MD5": False,
+                        "RC4-MD5": False,
+                        "RC4-SHA": True,
+                        "SEED-SHA": False}
+
+    policy_name = 'Mozilla-Security-Assurance-Ciphersuite-Policy-v-1-2'
 
     ami_map = json.load(open('config/ami_map.json', 'r'))
 
@@ -240,7 +302,7 @@ def create_stack(region,
                       'PolicyName': policy_name,
                       'PolicyTypeName': 'SSLNegotiationPolicyType'}
             conn_elb.build_complex_list_params(params, 
-                                               [(x, policy_attributes[x]) for x in policy_attributes.keys()],
+                                               [(x, "true" if policy_attributes[x] else "false") for x in policy_attributes.keys()],
                                                'PolicyAttributes.member',
                                                ('AttributeName', 'AttributeValue'))
             policy = conn_elb.get_list(action='CreateLoadBalancerPolicy', 
@@ -707,9 +769,6 @@ def point_dns_to_stack(region, stack_type, application, name):
 
 if __name__ == '__main__':
     path = "/identity/"
-
-    # region = 'us-west-1'
-    # availability_zones = ['b','c']
 
     region = 'us-west-2'
     availability_zones = ['a', 'b', 'c']
