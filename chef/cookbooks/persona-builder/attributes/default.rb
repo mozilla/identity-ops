8101c019f870ceab7005e70d23fa31da55c6fc65
Gene Wood <gene_wood@cementhorizon.com>
2014-06-05 16:25:49 -0700

    initial commit of persona-builder, a system to build identity stacks from

---
+++ chef/cookbooks/persona-builder/attributes/default.rb
@@ -0,0 +1,1 @@
+default["persona"]["builder"]["rpms"]["persona-manage_s3_secrets"] = "python-manage_s3_secrets-1.0.0-1.noarch.rpm"
---
+++ chef/cookbooks/persona-builder/recipes/default.rb
@@ -0,0 +1,25 @@
+#
+# Cookbook Name:: persona-builder
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "persona-common::default"
+
+package "python-psutil"
+package "python-gnupg"
+package "python-boto"
+
+
+rpm = node[:persona][:builder][:rpms]["persona-manage_s3_secrets"]
+
+remote_file "#{Chef::Config[:file_cache_path]}/#{rpm}" do
+   source "https://s3.amazonaws.com/mozilla-identity-us-standard/rpms/#{rpm}"
+end
+
+package "python-manage_s3_secrets" do
+  source "#{Chef::Config[:file_cache_path]}/#{rpm}"
+end
