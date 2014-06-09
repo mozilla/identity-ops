bdcd1959f2046a09fc21bbc8e6d105e0e61b73cf
Gene Wood <gene_wood@cementhorizon.com>
2014-06-09 13:54:36 -0700

    Use new data_bags

--- chef/solo.rb
+++ chef/solo.rb
@@ -1,5 +1,6 @@
 file_cache_path "/var/chef/cache"
 cookbook_path ["/root/identity-ops/chef/cookbooks", "/var/chef/cookbooks"]
+data_bag_path "/root/identity-ops/chef/data_bags"
 log_level :info 
 log_location "/var/log/chef.log" # or STDOUT 
 verbose_logging true
