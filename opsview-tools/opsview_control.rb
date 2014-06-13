7cb408dec366a353b07915a1f035c4fbf27f360b
Gene Wood <gene_wood@cementhorizon.com>
2014-06-13 10:16:00 -0700

    Licensing opsview_control

--- opsview-tools/opsview_control.rb
+++ opsview-tools/opsview_control.rb
@@ -1,7 +1,11 @@
 #!/usr/bin/ruby
 #
-# This will import or export your opsview settings
-#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+##
+# This will import or export your opsview settings or destroy all hosts in a hostgroup
 
 require 'rubygems'
 require 'rest_client'
