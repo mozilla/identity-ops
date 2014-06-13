930d62c35a49a1d3e3b79b123e5508dd21f6f46c
Gene Wood <gene_wood@cementhorizon.com>
2014-06-13 16:00:24 -0700

    https://github.com/mozilla/identity-ops/issues/155

--- chef/cookbooks/persona-dbwrite/attributes/default.rb
+++ chef/cookbooks/persona-dbwrite/attributes/default.rb
@@ -1,4 +1,4 @@
-default["persona"]["dbwrite"]["rpms"]["browserid-server"] = "browserid-server-0.2014.05.27-1.el6_129046.x86_64.rpm"
+default["persona"]["dbwrite"]["rpms"]["browserid-server"] = "browserid-server-0.2014.06.13-1.el6_129046.x86_64.rpm"
 
 default["persona"]["dbwrite"]["database"]["host"] = "dbmaster-example-123456.us-west-2.elb.amazonaws.com"
 default["persona"]["dbwrite"]["database"]["user"] = "browseridrw"
