3a89c56ed50f14b9ff13a232021e9e3b18ba4fb4
Daniel Thornton <dthornton@mozilla.com>
2014-06-16 16:09:58 -0700

    add stack_control to persona-builder chef

--- chef/cookbooks/persona-builder/recipes/default.rb
+++ chef/cookbooks/persona-builder/recipes/default.rb
@@ -23,3 +23,14 @@ end
 package "python-manage_s3_secrets" do
   source "#{Chef::Config[:file_cache_path]}/#{rpm}"
 end
+
+
+rpm = node[:persona][:builder][:rpms]["python-stack_control"]
+
+remote_file "#{Chef::Config[:file_cache_path]}/#{rpm}" do
+   source "https://s3.amazonaws.com/mozilla-identity-us-standard/rpms/#{rpm}"
+end
+
+package "python-stack_control" do
+  source "#{Chef::Config[:file_cache_path]}/#{rpm}"
+end
