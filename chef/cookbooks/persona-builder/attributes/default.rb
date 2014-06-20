3a21278204f8c8916cfa2738ce15e352a3e6c73f
Daniel Thornton <dthornton@mozilla.com>
2014-06-20 11:38:46 -0700

    bump stack_control version to 1.0.1-1

--- chef/cookbooks/persona-builder/attributes/default.rb
+++ chef/cookbooks/persona-builder/attributes/default.rb
@@ -1,2 +1,2 @@
 default["persona"]["builder"]["rpms"]["persona-manage_s3_secrets"] = "python-manage_s3_secrets-1.0.0-1.noarch.rpm"
-default["persona"]["builder"]["rpms"]["python-stack_control"] = "python-stack_control-1.0.0-1.noarch.rpm"
+default["persona"]["builder"]["rpms"]["python-stack_control"] = "python-stack_control-1.0.1-1.noarch.rpm"
