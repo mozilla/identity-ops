37e6c1f9c5e1152e958188e8908d408b7b28003b
Daniel Thornton <dthornton@mozilla.com>
2014-07-21 10:00:26 -0700

    update to persona train-2014.07.19 as per #168

--- chef/cookbooks/persona-dbwrite/attributes/default.rb
+++ chef/cookbooks/persona-dbwrite/attributes/default.rb
@@ -1,4 +1,4 @@
-default["persona"]["dbwrite"]["rpms"]["browserid-server"] = "browserid-server-0.2014.06.13-2.el6_129046.x86_64.rpm"
+default["persona"]["dbwrite"]["rpms"]["browserid-server"] = "browserid-server-0.2014.07.19-1.el6_130450.x86_64.rpm"
 
 default["persona"]["dbwrite"]["database"]["host"] = "dbmaster-example-123456.us-west-2.elb.amazonaws.com"
 default["persona"]["dbwrite"]["database"]["user"] = "browseridrw"
